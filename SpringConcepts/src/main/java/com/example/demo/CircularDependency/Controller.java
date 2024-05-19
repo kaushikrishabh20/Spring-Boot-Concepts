431c2254c7c8f7479d8aadb664cec99c1d6ebdc5
Rishabh <kaushikrishabh20@gmail.com>
2024-05-20 01:02:11 +0530

    Refactoring

--- SpringConcepts/src/main/java/com/example/demo/CircularDependency/Controller.java
+++ SpringConcepts/src/main/java/com/example/demo/CircularDependency/Controller.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package com.example.demo.CircularDependency;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.CircularDependency.Lazt.MyBean3;
+import com.example.demo.CircularDependency.Lazy.MyBean3;
 import com.example.demo.CircularDependency.PostConstruct.MyBean;
 
 @RestController
--- SpringConcepts/src/main/java/com/example/demo/CircularDependency/Lazy/MyBean3.java
+++ SpringConcepts/src/main/java/com/example/demo/CircularDependency/Lazy/MyBean3.java
@@ -1,4 +1,4 @@
-package com.example.demo.CircularDependency.Lazt;
+package com.example.demo.CircularDependency.Lazy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
